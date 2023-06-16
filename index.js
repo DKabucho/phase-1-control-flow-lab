5dba4c49d7b067ba95193310b22485530488b42b
DKabucho <danielkabucho2004@gmail.com>
2023-06-16 14:49:07 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if(depth <=400 ){
+    scuberGreetingForFeet="This one is on me!"
+  }
+  else(scuberGreetingForFeet="I will gladly take your thirty bucks.")
+  if(depth>=2500){
+    scuberGreetingForFeet ="No can do."
+  }
+  return scuberGreetingForFeet
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return ("Ok, sounds good.")
+  }
+  else {
+    return ("No go.")
+  }
+ 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  if(tips==="generous") {
+    return("Thank you so much.")
+  }
+  if (tips==="not as generous") {
+    return("Thank you.")
+  } else {
+    return("Bye.")
+  }
 }
